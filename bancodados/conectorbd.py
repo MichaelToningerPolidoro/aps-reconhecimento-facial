5e90ea13ce10dba8fb5552a2347b6a9e8307c85b
Michael Toninger <michaeltp.2012@hotmail.com>
2021-10-26 11:32:00 -0300

    Adicionado connector com o banco de dados

--- bancodados/conectorbd.py
+++ bancodados/conectorbd.py
@@ -1,8 +1,18 @@
 import configparser
+import mysql.connector
 
 config = configparser.RawConfigParser()
 
 config.read('config.config')
-print(config.get('database', 'url'))
-print(config.get('database', 'user'))
-print(config.get('database', 'password'))
+
+db = mysql.connector.connect(
+    host=config.get('database', 'url'),
+    user=config.get('database', 'user'),
+    passwd=config.get('database', 'password')
+)
+
+cursor = db.cursor()
+
+cursor.execute('CREATE DATABASE IF NOT EXISTS testepython')
+
+db.close()
---
+++ bancodados/setup.py
@@ -0,0 +1,2 @@
+# Script criado para executar um arquivo mysql e deixar o banco de dados na maneira correta
+# para que seja possivel utilizar a aplicação de maneira correta.
