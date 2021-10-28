3c5c1d77b5f0d3c11a2377afdd70747b26017074
Michael Toninger <michaeltp.2012@hotmail.com>
2021-10-28 09:19:53 -0300

    Transformado script bd em uma classe

--- banco_dados/bd.py
+++ banco_dados/bd.py
@@ -1,17 +1,35 @@
 import configparser
 import mysql.connector
 
-config = configparser.RawConfigParser()
-config.read('config.config')
-bdurl = config.get('database', 'url')
-bdusuario = config.get('database', 'user')
-bdsenha = config.get('database', 'password')
-bdnome = 'testepython'
+class Bd:
+    _bdurl: str = None
+    _bdusuario: str = None
+    _bdsenha: str = None
+    _bdnome: str = None
+    _conexaoBd = None
 
-def buscarNomePessoaReconhecida(idPessoaReconhecida: int) -> str:
-    bd = mysql.connector.connect(host=bdurl, user=bdusuario, passwd=bdsenha, database=bdnome)
-    cursor = bd.cursor()
-    cursor.execute(f'SELECT nome FROM teste where id = {idPessoaReconhecida}')
-    nome = cursor.fetchone()[0]
-    bd.close()
-    return nome
+    def __init__(self):
+        config = configparser.RawConfigParser()
+        config.read('config.config')
+        self._bdurl = config.get('database', 'url')
+        self._bdusuario = config.get('database', 'user')
+        self._bdsenha = config.get('database', 'password')
+        self._bdnome = 'testepython'
+
+    def conectarBd(self):
+        self._conexaoBd = mysql.connector.connect(
+            host=self._bdurl, user=self._bdusuario, passwd=self._bdsenha, database=self._bdnome
+        )
+
+        return self._conexaoBd.cursor()
+
+    def executarQuery(self, query: str):
+        cursor = self.conectarBd()
+        cursor.execute(query)
+        resultado = cursor.fetchone()
+        self._conexaoBd.close()
+        return resultado
+
+    def obterNomePessoaReconhecida(self, idPessoaReconhecida: int) -> str:
+        resultado = self.executarQuery(f'SELECT nome FROM teste WHERE id={idPessoaReconhecida};')
+        return resultado[0]
